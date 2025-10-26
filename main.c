6364a7d0572cfa35e2cc225e938727850b2834b8
sasagawa888 <ken-s@tkcnf.or.jp>
2025-10-26 10:33:55 +0900

    update version

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ written by kenichi sasagawa
 #include <signal.h>
 #include "scm3.h"
 
-double version = 1.20;
+double version = 1.30;
 cell heap[HEAPSIZE];
 int stack[STACKSIZE];
 int argstk[STACKSIZE];
