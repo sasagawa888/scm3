498dda9b710a663eb648cf29b6d0e51e8954a4bf
sasagawa888 <ken-s@tkcnf.or.jp>
2025-11-01 12:12:51 +0900

    add edwin folder

---
+++ edwin/compat/curses_stubs.h
@@ -0,0 +1,5 @@
+#ifndef COMPAT_CURSES_STUBS_H
+#define COMPAT_CURSES_STUBS_H
+#endif 
+#include <ncurses.h>
+
---
+++ edwin/syn_highlight.c
@@ -0,0 +1,129 @@
+#include <string.h>
+#include <stdbool.h>
+#include "term.h"
+
+#define NELEM(X) (sizeof(X) / sizeof((X)[0]))
+
+// special form token. use in Edlis indentation
+static const char *special[] = {
+    "define", "let", "let*", "letrec", "case",
+    "do", "begin", 
+};
+
+// syntax token
+static const char *syntax[] = {
+    "lambda", "letrec", "let", "let*", "set!", "define",
+    "dynamic", "set-dynamic", "function", "function*", "symbol-function",
+    "and", "or", "if", "cond", "do", "begin", "case",
+    "trace", "untrace",
+};
+
+// builtin token
+static const char *builtin[] = {
+    "-", "*", "/=", "+", "<", "<=", "=", ">", ">=",
+    "abs", "append", "apply", "assoc", "atan",
+    "atom?", "vector?", "car", "cdr", "char<?", "char<=?", "char=?",
+    "char>?", "char>=?", "character?", "cons", "pair?", 
+    "cos", "cosh", "eq?", "eqv?", "equal?", "error",
+    "eval", "exp", "float?", "gbc", "gcd", "integer?",
+    "isqrt", "lcm", "length", "list",
+    "list?", "load", "log", "map", "for-each", "max", "member", "min", "mod",
+    "not", "null?", "number?", "write", "display", "property", "quit", "quotient", 
+    "read-char", "read", "reverse", "round",
+    "set-car!", "set-cdr!", "sin", "sinh", "sqrt",
+    "string/=?", "string<?", "string<=?", "string=?", "string>?", "string>=?", "string",
+    "symbol?", "tan", "tanh", "vector", 
+};
+
+// extended function
+static const char *extended[] = {
+    
+};
+
+static bool in_syntax_table(const char *str)
+{
+    int i;
+
+    for (i = 0; i < (int) NELEM(syntax); i++) {
+	if (strcmp(syntax[i], str) == 0) {
+	    return true;
+	}
+    }
+    return false;
+}
+
+static bool in_builtin_table(const char *str)
+{
+    int i;
+
+    for (i = 0; i < (int) NELEM(builtin); i++) {
+	if (strcmp(builtin[i], str) == 0) {
+	    return true;
+	}
+    }
+    return false;
+}
+
+static bool in_extended_table(const char *str)
+{
+    int i;
+
+    for (i = 0; i < (int) NELEM(extended); i++) {
+	if (strcmp(extended[i], str) == 0) {
+	    return true;
+	}
+    }
+    return false;
+}
+
+bool in_special_table(const char *str)
+{
+    int i;
+
+    for (i = 0; i < (int) NELEM(special); i++) {
+	if (strcmp(special[i], str) == 0) {
+	    return true;
+	}
+    }
+    return false;
+}
+
+enum HighlightToken maybe_match(const char *str)
+{
+    if (in_syntax_table(str)) {
+	return HIGHLIGHT_SYNTAX;
+    }
+    if (in_builtin_table(str)) {
+	return HIGHLIGHT_BUILTIN;
+    }
+    if (in_extended_table(str)) {
+	return HIGHLIGHT_EXTENDED;
+    }
+    return HIGHLIGHT_NONE;
+}
+
+void
+gather_fuzzy_matches(const char *str, const char *candidates[],
+		     int *candidate_pt)
+{
+    int i;
+
+    for (i = 0; i < (int) NELEM(syntax); i++) {
+	if (strstr(syntax[i], str) != NULL && syntax[i][0] == str[0]) {
+	    candidates[*candidate_pt] = syntax[i];
+	    *candidate_pt = (*candidate_pt) + 1;
+	}
+    }
+    for (i = 0; i < (int) NELEM(builtin); i++) {
+	if (strstr(builtin[i], str) != NULL && builtin[i][0] == str[0]) {
+	    candidates[*candidate_pt] = builtin[i];
+	    *candidate_pt = (*candidate_pt) + 1;
+	}
+    }
+    for (i = 0; i < (int) NELEM(extended); i++) {
+	if (strstr(extended[i], str) != NULL && extended[i][0] == str[0]) {
+	    candidates[*candidate_pt] = extended[i];
+	    *candidate_pt = (*candidate_pt) + 1;
+	}
+    }
+}
