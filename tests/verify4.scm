9acdc048182f84669f97a4411ca6f143d17953fd
sasagawa888 <ken-s@tkcnf.or.jp>
2025-11-04 11:26:08 +0900

    update verify4.scm

--- tests/verify4.scm
+++ tests/verify4.scm
@@ -47,3 +47,34 @@
 (test '(catch-error '(if 1 2 3 4)) 20)
 (test '(catch-error '(if 1)) 20)
 
+
+;; --- arity mismatch ---
+(test '(catch-error '(if 1 2 3 4)) 20)
+(test '(catch-error '(if 1)) 20)
+(test '(catch-error '(car)) 11)
+(test '(catch-error '(cdr 1 2)) 11)
+(test '(catch-error '(cons 1 2 3)) 12)
+
+;; --- type mismatch ---
+(test '(catch-error '(+ "abc" 1)) 4)
+(test '(catch-error '(string-length 42)) 3)
+(test '(catch-error '(vector-ref 123 0)) 7)
+(test '(catch-error '(eq? "a" 1)) #f)
+
+;; --- undefined variable ---
+(test '(catch-error 'x) 15)
+;(test '(catch-error '(+ a b)) 15)
+
+;; --- division by zero ---
+(test '(catch-error '(/ 1 0)) 19)
+
+;; --- syntax error ---
+(test '(catch-error '(lambda x x)) 5)
+;(test '(catch-error '(define (foo x y))) 24)
+;(test '(catch-error '(begin 1 . 2)) 24)
+
+;; --- normal cases (no error) ---
+(test '(catch-error '(substring "abcd" 1 3)) "bc")
+(test '(catch-error '(vector-ref #(1 2 3) 2)) 3)
+(test '(catch-error '(if #t 1 2)) 1)
+
